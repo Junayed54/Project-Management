c15affdc16effd8833871508a46c61161671cdfd
Junayed-shanto <junayedshanto54@gmail.com>
2024-06-24 22:55:44 +0600

    projects app views and urls define has done

--- projectMingle/projects/urls.py
+++ projectMingle/projects/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import ProjectListCreateAPIView, ProjectRetrieveUpdateDestroyAPIView
 
-urlpatterns =[
-    
+urlpatterns = [
+    path('projects/', ProjectListCreateAPIView.as_view(), name='project-list-create'),
+    path('projects/<uuid:pk>/', ProjectRetrieveUpdateDestroyAPIView.as_view(), name='project-detail'),
 ]
--- projectMingle/projects/views.py
+++ projectMingle/projects/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Project
+from .serializers import ProjectSerializer
 
-# Create your views here.
+class ProjectListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class ProjectRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
